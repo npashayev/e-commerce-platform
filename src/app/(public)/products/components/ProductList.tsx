222ff22bb6dae424779586e47ecb610a8b98966f
Namig <namig.pashayev1@gmail.com>
2026-01-14 20:55:13 +0400

    Create ProductList component

--- src/app/(public)/products/components/ProductList.tsx
+++ src/app/(public)/products/components/ProductList.tsx
@@ -1,32 +1,19 @@
-import { getAllProducts } from '@/app/actions/product/getAllProducts';
 import styles from './products.module.scss';
 import ProductCard from './ProductCard';
+import { getProducts } from '@/lib/api/products';
 
-interface Props {
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}
+const ProductList = async () => {
+  const products = await getProducts();
 
-const ProductList = async ({ searchParams }: Props) => {
-    const params = (await searchParams) || {};
-
-    const products = await getAllProducts({
-        limit: params.limit ? Number(params.limit) : undefined,
-        skip: params.skip ? Number(params.skip) : undefined,
-        category: typeof params.category === 'string' ? params.category : undefined,
-        searchQuery: typeof params.search === 'string' ? params.search : undefined,
-        priceSort: params.priceSort === 'asc' || params.priceSort === 'desc' ? params.priceSort : undefined,
-        ratingSort: params.ratingSort === 'asc' || params.ratingSort === 'desc' ? params.ratingSort : undefined,
-    });
-
-    return (
-        <main className={styles.main}>
-            <div className={styles.productsCnr}>
-                {products.map(product => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
-        </main>
-    );
+  return (
+    <main className={styles.main}>
+      <div className={styles.productsCnr}>
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default ProductList;
