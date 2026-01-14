9cd2071f0beeee564a462f19ee8f59d009238c59
Namig <namig.pashayev1@gmail.com>
2026-01-14 20:55:03 +0400

    Create Reviews component

---
+++ src/app/(public)/products/[productId]/components/Reviews.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import styles from './reviews.module.scss';
+import star from '@/assets/star.png';
+import { use } from 'react';
+import { getReviewsByProductId } from '@/lib/api/products';
+
+interface Props {
+  productId: string;
+}
+
+const Reviews = ({ productId }: Props) => {
+  const reviews = use(getReviewsByProductId(productId));
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  };
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.heading}>Reviews & Rating</div>
+      <div className={styles.reviewsCnr}>
+        {reviews ? (
+          reviews.map((review, i) => (
+            <div key={i} className={styles.review}>
+              <div className={styles.ratingCnr}>
+                <div className={styles.starCnr}>
+                  <Image src={star} fill className={styles.star} alt="star" />
+                </div>
+                <div className={styles.rating}>{review.rating}</div>
+              </div>
+              <div className={styles.body}>
+                <div className={styles.userInfo}>
+                  <div className={styles.userName}>{review.reviewerName}</div>
+                  <div className={styles.date}>{formatDate(review.date)}</div>
+                </div>
+                <div className={styles.comment}>{review.comment}</div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No reviews for this product</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
