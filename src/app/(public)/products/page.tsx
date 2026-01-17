f95efebe2b8ca26e75457e78cd86ed5760998618
Namig <namig.pashayev1@gmail.com>
2026-01-17 23:59:07 +0400

    Add FilterBar to products page

--- src/app/(public)/products/page.tsx
+++ src/app/(public)/products/page.tsx
@@ -2,6 +2,7 @@ import { use } from 'react';
 import Categories from './components/Categories';
 import ProductList from './components/ProductList';
 import styles from './page.module.scss';
+import FilterBar from './components/FilterBar';
 
 interface Props {
   searchParams: Promise<{
@@ -13,6 +14,7 @@ const ProductsPage = ({ searchParams }: Props) => {
   const { category } = use(searchParams);
   return (
     <div className={styles.page}>
+      <FilterBar />
       <div className={styles.content}>
         <Categories category={category} />
         <ProductList />
