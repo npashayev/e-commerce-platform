a130899f23cca8701c08a953c3621ab4eb9b2135
Namig <namig.pashayev1@gmail.com>
2026-01-17 18:36:58 +0400

    Update FormField.tsx

--- src/components/shared/FormField.tsx
+++ src/components/shared/FormField.tsx
@@ -1,20 +1,43 @@
 import styles from '@/styles/resource-form.module.scss';
 
-interface Props {
+interface BaseProps {
   label: string;
   value?: string | number | null;
+  type?: string;
+  step?: string;
+  required?: boolean;
 }
 
-export const InfoField = ({ label, value }: Props) => (
+interface InfoFieldProps extends Omit<BaseProps, 'name'> {}
+
+interface EditFieldProps extends BaseProps {
+  name: string;
+}
+
+export const InfoField = ({ label, value }: InfoFieldProps) => (
   <div className={styles.inputCnr}>
     <label>{label}</label>
     <p className={styles.info}>{value || ''}</p>
   </div>
 );
 
-export const EditField = ({ label, value }: Props) => (
+export const EditField = ({
+  label,
+  value,
+  name,
+  type = 'text',
+  step,
+  required,
+}: EditFieldProps) => (
   <div className={styles.inputCnr}>
     <label>{label}</label>
-    <p className={styles.info}>{value || ''}</p>
+    <input
+      className={styles.info}
+      name={name}
+      type={type}
+      step={step}
+      defaultValue={value || ''}
+      required={required}
+    />
   </div>
 );
