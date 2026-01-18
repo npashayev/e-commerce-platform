88e215c7f9c97e02770494bf612df6b060e2f222
Namig <namig.pashayev1@gmail.com>
2026-01-18 23:45:26 +0400

    Add type declarations for AppDispatch and RootState

--- src/lib/types/global.d.ts
+++ src/lib/types/global.d.ts
@@ -1,2 +1,8 @@
 declare module "*.css"
 declare module "*.scss"
+
+
+declare global {
+  type AppDispatch = typeof import('@/lib/store/store').store.dispatch;
+  type RootState = ReturnType<typeof import('@/lib/store/store').store.getState>;
+}
