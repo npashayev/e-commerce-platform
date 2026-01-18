885b65b8a3385cf50d92f1602d0c62277c6507fa
Namig <namig.pashayev1@gmail.com>
2026-01-18 23:51:13 +0400

    Create userSlice

---
+++ src/lib/store/slices/userSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { SafeUser } from '@/lib/types/auth';
+
+interface UserState {
+  user: SafeUser | null;
+}
+
+const initialState: UserState = {
+  user: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<SafeUser>) => {
+      state.user = action.payload;
+    },
+    clearUser: (state) => {
+      state.user = null;
+    },
+    updateUser: (state, action: PayloadAction<Partial<SafeUser>>) => {
+      if (state.user) {
+        state.user = { ...state.user, ...action.payload };
+      }
+    },
+  },
+});
+
+export const { setUser, clearUser, updateUser } = userSlice.actions;
+export default userSlice.reducer;
