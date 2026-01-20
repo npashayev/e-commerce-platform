7f2764fb3eae25aa35df0f3850772f7cacd8003f
Namig <namig.pashayev1@gmail.com>
2026-01-20 20:36:00 +0400

    Replace the prompt with constant prompt

--- src/app/actions/ai/getReviewForProduct.ts
+++ src/app/actions/ai/getReviewForProduct.ts
@@ -1,19 +1,11 @@
 'use server';
 import { gemini } from "@/lib/ai/gemini";
 import { Product } from "@prisma/client";
+import { AI_PRODUCT_REVIEW_PROMPT } from "@/lib/constants/prompts";
 
 export const getAiReviewForProduct = async (product: Product) => {
-    const prePrompt = `
-        You are a professional product reviewer. You will be provided with json object of a product to be reviewed.
-        By carefully analyzing the product info, you will respond with detailed review about the product.
-        Your review will include 3 sections: 
-        1. General review based on product data
-        2. Review based on shipping and warranty information
-        3. Price considerations: Will it worth to buy this product for this price.
-        BE PROFESSIONAL, HONEST AND CLEAR IN YOUR RESPONSE.
-        `;
     const productJson = JSON.stringify(product);
-    const fullPrompt = prePrompt + `PRODUCT JSON: \n ${productJson}`;
+    const fullPrompt = AI_PRODUCT_REVIEW_PROMPT + `PRODUCT JSON: \n ${productJson}`;
 
     try {
         const response = await gemini.models.generateContent({
