a6049b8e909f5747dc1ab676a8b6c240fc55b096
Namig <namig.pashayev1@gmail.com>
2026-01-22 21:51:13 +0400

    Add type declarations for image files

--- src/lib/types/global.d.ts
+++ src/lib/types/global.d.ts
@@ -1,5 +1,12 @@
 declare module "*.css"
 declare module "*.scss"
+declare module "*.png"
+declare module "*.jpg"
+declare module "*.jpeg"
+declare module "*.gif"
+declare module "*.svg"
+declare module "*.webp"
+declare module "*.ico"
 
 
 declare global {
