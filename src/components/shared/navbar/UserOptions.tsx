ec31c196c13f39f6dab57e6b7ea7e0e050375015
Namig <namig.pashayev1@gmail.com>
2026-01-22 20:39:23 +0400

    Fix the bug with the wrong placement of click outside handler

--- src/components/shared/navbar/UserOptions.tsx
+++ src/components/shared/navbar/UserOptions.tsx
@@ -3,15 +3,18 @@ import styles from './user-options.module.scss';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useAuth } from '@/lib/hooks/useAuth';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import UserDropdown from './UserDropdown';
+import useClickOutside from '@/lib/hooks/useClickOutside';
 
 const UserOptions = () => {
   const { user } = useAuth();
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  useClickOutside([{ contentRef: ref, onClickOutside: () => setDropdownOpen(false) }]);
 
   return (
-    <div className={styles.userMenu}>
+    <div ref={ref} className={styles.userMenu}>
       <button
         className={styles.userNameCnr}
         onClick={() => setDropdownOpen(prev => !prev)}
